56783c8150df72c1c767d85aa5febf14c43d5654
Taehwan Kim <obtusa07@gmail.com>
2023-08-15 18:20:44 +0900

    add: Home Screen

---
+++ lib/home.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: SizedBox(
+            width: double.infinity,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.1),
+                      Text(
+                        'MENSETSU\nHelper',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 36,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.1),
+                      Image.asset('assets/images/applicant_asian_man.png'),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 32,
+                  left: 24,
+                  right: 24,
+                  child: GestureDetector(
+                    onTap: () {
+                      print('asdf');
+                    },
+                    child: Container(
+                      color: Colors.blue,
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      height: 58,
+                      child: Text(
+                        '시작하기',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
