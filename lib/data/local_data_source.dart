0700f0558dca16adba0747bb9e2d00a4e0c9b89a
Taehwan Kim <obtusa07@gmail.com>
2023-08-28 21:40:24 +0900

    feat: dummy LocalDataSource and repository and viewModel

---
+++ lib/data/local_data_source.dart
@@ -0,0 +1,11 @@
+class LocalDataSource {
+  Future<List<String>> getResultTitle() async {
+    // 슈도 코드
+    return [
+      "Total Mensetsu Time",
+      "Average Response Time",
+      "Longest Response Time",
+      "Shortest Response Time"
+    ];
+  }
+}
